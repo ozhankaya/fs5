daa72cfd665da66136a10f9fff90eafe337f05ec
ozhankaya <ozhankaya07@gmail.com>
2023-10-07 16:46:59 +0300

    all in one

---
+++ src/components/Sections/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-scroll";
+// Assets
+import LogoImg from "../../assets/svg/Logo";
+import NewLogoIcon from "../../assets/img/logowhite.png";
+
+export default function Contact() {
+
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  }
+
+  return (
+    <Wrapper>
+      <div className="darkBg">
+        <div className="container">
+          <InnerWrapper className="flexSpaceCenter" style={{ padding: "30px 0" }}>
+            <Link className="flexCenter animate pointer" to="home" smooth={true} offset={-80}>
+              <img src={NewLogoIcon} width="100px" height="50px" />
+
+            </Link>
+            <StyleP class="whiteColor font13">
+              <span className="purpleColor font13">© 2023 |FS5 All Right Reserved</span> 
+            </StyleP>
+
+            <Link className="whiteColor animate pointer font13" to="home" smooth={true} offset={-80}>
+              Back to top
+            </Link>
+          </InnerWrapper>
+        </div>
+      </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+const InnerWrapper = styled.div`
+  @media (max-width: 550px) {
+    flex-direction: column;
+  }
+`;
+const StyleP = styled.p`
+  @media (max-width: 550px) {
+    margin: 20px 0;
+  }
+`;
---
+++ src/components/Sections/Header.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+import styled from "styled-components";
+// Components
+import FullButton from "../Buttons/FullButton";
+// Assets
+import HeaderImage from "../../assets/img/header-img.png";
+import QuotesIcon from "../../assets/svg/Quotes";
+import Dots from "../../assets/svg/Dots";
+// import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <Wrapper id="home" className="container flexSpaceCenter">
+      <LeftSide className="flexCenter">
+        <div>
+          <h1 className="extraBold font60">We are Digital Development Agency.</h1>
+          <HeaderP className="font13 semiBold">
+            We provide high quality, affordable End2End web pages and applications development with all important javascript frameworks React.js, Angular and Vue.js.
+          </HeaderP>
+          <a href="https://www.upwork.com/agencies/1707450238761168896/" target="_blank">
+          <BtnWrapper>
+            <FullButton title="Upwork Agency">  </FullButton>
+          </BtnWrapper>
+          </a>
+        </div>
+      </LeftSide>
+      <RightSide>
+        <ImageWrapper>
+          <Img className="radius8" src={HeaderImage} alt="office" style={{zIndex: 9}} />
+          <QuoteWrapper className="flexCenter darkBg radius8">
+            <QuotesWrapper>
+              <QuotesIcon />
+            </QuotesWrapper>
+            <div>
+              <p className="font15 whiteColor">
+                <em>Our team has 15 years experienced business development managers and web developers.</em>
+              </p>
+              <p className="font13 orangeColor textRight" style={{marginTop: '10px'}}>FS5</p>
+            </div>
+          </QuoteWrapper>
+          asdfasf
+          <DotsWrapper>
+            <Dots />
+          </DotsWrapper>
+        </ImageWrapper>
+        <GreyDiv className="lightBg"></GreyDiv>
+      </RightSide>
+    </Wrapper>
+  );
+}
+
+
+const Wrapper = styled.section`
+  padding-top: 80px;
+  width: 100%;
+  min-height: 840px;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+const LeftSide = styled.div`
+  width: 50%;
+  height: 100%;
+  @media (max-width: 960px) {
+    width: 100%;
+    order: 2;
+    margin: 50px 0;
+    text-align: center;
+  }
+  @media (max-width: 560px) {
+    margin: 80px 0 50px 0;
+  }
+`;
+const RightSide = styled.div`
+  width: 50%;
+  height: 100%;
+  @media (max-width: 960px) {
+    width: 100%;
+    order: 1;
+    margin-top: 30px;
+  }
+`;
+const HeaderP = styled.div`
+  max-width: 470px;
+  padding: 15px 0 50px 0;
+  line-height: 1.5rem;
+  @media (max-width: 960px) {
+    padding: 15px 0 50px 0;
+    text-align: center;
+    max-width: 100%;
+  }
+`;
+const BtnWrapper = styled.div`
+  max-width: 190px;
+  @media (max-width: 960px) {
+    margin: 0 auto;
+  }
+`;
+const GreyDiv = styled.div`
+  width: 30%;
+  height: 700px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 0;
+  @media (max-width: 960px) {
+    display: none;
+  }
+`;
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  position: relative;
+  z-index: 9;
+  @media (max-width: 960px) {
+    width: 100%;
+    justify-content: center;
+  }
+`;
+const Img = styled.img`
+  @media (max-width: 560px) {
+    width: 80%;
+    height: auto;
+  }
+`;
+const QuoteWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 50px;
+  max-width: 330px;
+  padding: 30px;
+  z-index: 99;
+  @media (max-width: 960px) {
+    left: 20px;
+  }
+  @media (max-width: 560px) {
+    bottom: -50px;
+  }
+`;
+const QuotesWrapper = styled.div`
+  position: absolute;
+  left: -20px;
+  top: -10px;
+`;
+const DotsWrapper = styled.div`
+  position: absolute;
+  right: -100px;
+  bottom: 100px;
+  z-index: 2;
+  @media (max-width: 960px) {
+    right: 100px;
+  }
+  @media (max-width: 560px) {
+    display: none;
+  }
+`;
+
+
